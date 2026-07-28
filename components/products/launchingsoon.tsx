3865b638e48415045ea72d090a4f89cb3d66fa20
thrun-11 <91716287+thrun-11@users.noreply.github.com>
2026-07-28 13:54:00 +0530

    latest:coming soon

--- components/products/launchingsoon.tsx
+++ components/products/launchingsoon.tsx
@@ -14,7 +14,7 @@ const LaunchingSoon = () => {
             </div>
             <div className="flex flex-col items-center justify-center z-10 px-4 text-center py-20 w-full">
                 <h2 className="text-[15vw] sm:text-[11vw] lg:text-[8.5vw] xl:text-[10rem] leading-[1.05] font-sans font-medium tracking-tighter uppercase text-transparent bg-clip-text bg-gradient-to-b  from-[#e5e0e0] via-[#ef8f60] to-[#f8e780] pb-1 select-none">
-                    <span className="block sm:inline">LAUNCHING</span>{' '}
+                    <span className="block sm:inline">COMING</span>{' '}
                     <span className="block sm:inline">SOON</span>
                 </h2>
             </div>
